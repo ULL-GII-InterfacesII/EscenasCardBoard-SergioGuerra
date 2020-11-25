0dc71d10834d5a26920b7f9c955d36ea8653cf02
alu0101133201 <50806571+alu0101133201@users.noreply.github.com>
2020-11-25 17:59:54 +0000

    Scripts

---
+++ scripts/deleteOnClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deleteOnClick : MonoBehaviour
+{
+  public int cubes = 0;
+
+  public void delete() {
+    Destroy(gameObject);
+    cubes++;
+  }
+}
